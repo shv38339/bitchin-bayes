11eb45fd0fced411d2eef17d1e5579ce9d736ae5
shv38339 <eleetsvalenzuela@gmail.com>
2016-04-25 06:03:27 -0400

    file re-organization

--- ar1.R
+++ ar1.R
@@ -1,3 +1,8 @@
+# dependencies ----
+library(rjags)
+library(R2jags)
+library(dplyr)
+
 # ar1 commands from the following link ----
 # https://onlinecourses.science.psu.edu/stat510/node/61
 # but don't know what to do with this information 
@@ -12,7 +17,7 @@ lag1.plot(x, 1) # we don't see any relationship
 acf(x, xlim = c(1, 82))
 xlag1 <- lag(x, -1)
 y <- cbind(x, xlag1)
-ar1fit <- lm(y[, 1] ~ y[, 2])
+ar1fit <- lm(y[, 1] ~ y[, 2]) # ar1fit here but didn't know it
 summary(ar1fit)
 
 # code below from Mark on assistance with ARIMA ----
@@ -31,7 +36,7 @@ parnames <- c("sigma", "rho")
 set.seed(124)
 mod <- jags.parallel(data = jags.data, parameters.to.save = parnames, 
                      n.chains = 4, n.burnin = 1000, n.iter = 1000 + 30000, n.thin = 30,
-                     model.file = "jags-code/mod.txt")
+                     model.file = "jags-txt/mod.txt")
 # jags diagnostics
 max(mod$BUGSoutput$summary[, c("Rhat")])
 min(mod$BUGSoutput$summary[, c("n.eff")])
